7ce04206d1dca2f489899614d65aff4b80e7244c
Brandon Catubig <bcatubig@pm.me>
2020-08-22 18:34:26 -0500

    [task] add start and stop methods

--- simple-task-scheduler/task.go
+++ simple-task-scheduler/task.go
@@ -1,14 +1,22 @@
 package taskscheduler
 
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
 const (
 	UNSCHEDULED = "unscheduled"
 	SCHEDULED   = "scheduled"
 	RUNNING     = "running"
+	STOPPED     = "stopped"
 	FAILED      = "failed"
 )
 
 // Task is an individual unit of work
 type Task struct {
+	id       string
 	name     string
 	status   string
 	unitSize int
@@ -17,8 +25,23 @@ type Task struct {
 // NewTask returns a new, unscheduled task
 func NewTask(name string, unitSize int) *Task {
 	return &Task{
+		id:       uuid.New().String(),
 		name:     name,
 		status:   UNSCHEDULED,
 		unitSize: unitSize,
 	}
+}
+
+// Start starts a task
+func (t *Task) Start() error {
+	fmt.Printf("Starting task: %s <%s>", t.name, t.id)
+	t.status = RUNNING
+	return nil
+}
+
+// Stop stops a task
+func (t *Task) Stop() error {
+	fmt.Printf("Stopping task: %s <%s>", t.name, t.id)
+	t.status = STOPPED
+	return nil
 }
