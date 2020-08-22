d159e2e2ba928fdc73e43031f131b6cd668eb846
Brandon Catubig <bcatubig@pm.me>
2020-08-22 18:34:45 -0500

    [worker] add addTask method

--- simple-task-scheduler/worker.go
+++ simple-task-scheduler/worker.go
@@ -1,13 +1,17 @@
 package taskscheduler
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 // Worker executes tasks
 type Worker struct {
 	id                 string
 	maxWorkUnits       int
 	availableWorkUnits int
-	tasks              []Task
+	tasks              []*Task
 }
 
 // NewWorker runs an empty Worker with a max task capacity of zero
@@ -16,6 +20,24 @@ func NewWorker(maxWorkUnits int) *Worker {
 		id:                 uuid.New().String(),
 		maxWorkUnits:       maxWorkUnits,
 		availableWorkUnits: maxWorkUnits,
-		tasks:              []Task{},
+		tasks:              []*Task{},
 	}
+}
+
+// AvailableWorkUnits returns the number of available work units that can be
+// used for tasks
+func (w Worker) AvailableWorkUnits() int {
+	return w.availableWorkUnits
+}
+
+// AddTask will add a new Task to a Worker
+//
+// An error will be returned if the task fails to be added
+func (w *Worker) AddTask(t *Task) error {
+	if t.unitSize <= w.maxWorkUnits {
+		w.tasks = append(w.tasks, t)
+		t.status = SCHEDULED
+	}
+
+	return fmt.Errorf("not able to add task")
 }
