ca9313dbbd5ad03e7aba9398ac7ad3b3803f25a3
Brandon Catubig <bcatubig@pm.me>
2020-08-22 20:27:29 -0500

    [scheduler] add logging

--- simple-task-scheduler/scheduler.go
+++ simple-task-scheduler/scheduler.go
@@ -1,21 +1,43 @@
 package taskscheduler
 
 import (
+	"errors"
 	"fmt"
+
+	"github.com/hashicorp/go-hclog"
 
 	"github.com/google/uuid"
 )
+
+// SchedulerOptions provides overridable options
+type SchedulerOptions struct {
+	ID     string
+	Logger hclog.Logger
+}
 
 // Scheduler schedule units of work to Worker nodes
 type Scheduler struct {
 	id      string
 	workers []*Worker
+	logger  hclog.Logger
 }
 
-func NewScheduler() *Scheduler {
+func NewScheduler(options *SchedulerOptions) *Scheduler {
+	if options == nil {
+		options = &SchedulerOptions{
+			ID:     uuid.New().String(),
+			Logger: hclog.NewNullLogger(),
+		}
+	}
+
+	if options.ID == "" {
+		options.ID = uuid.New().String()
+	}
+
 	return &Scheduler{
-		id:      uuid.New().String(),
+		id:      options.ID,
 		workers: []*Worker{},
+		logger:  options.Logger,
 	}
 }
 
@@ -27,6 +49,7 @@ func (s *Scheduler) AddWorker(w *Worker) error {
 		return fmt.Errorf("scheduler: %s already contains worker: %s", s.id, w.id)
 	}
 	s.workers = append(s.workers, w)
+	s.logger.Info("worker added", "scheduler_id", s.id, "worker_id", w.id)
 	return nil
 }
 
@@ -41,5 +64,18 @@ func containsWorker(s *Scheduler, w *Worker) bool {
 
 // AddTask will create a new task to be scheduled on a worker
 func (s *Scheduler) AddTask(t *Task) error {
-	return nil
+	for _, w := range s.workers {
+		err := w.AddTask(t)
+
+		if err != nil {
+			s.logger.Info("not able to add task to worker", "worker_id", w.id, "error", err)
+			continue
+		}
+
+		s.logger.Info("task added to worker", "task_id", t.id, "worker_id", w.id)
+		return nil
+	}
+
+	s.logger.Error("no workers available to schedule task", "task_id", t.id)
+	return errors.New("no workers available")
 }
